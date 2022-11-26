9e4ee56830eb6e0bb73f961364b8c7d87ed7b681
Varshu215 <119064037+Varshu215@users.noreply.github.com>
2022-11-26 21:14:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './form';
+import InitDemo from './InitDemo';
+import Lifecycle from './Lifecycle';
+ 
+function App() {
+  return (
+    //<InitDemo/>
+    <Lifecycle/>
+      
+  );
+}
+
+export default App;
